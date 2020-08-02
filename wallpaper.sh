54bee1b50253051cc80eb3460cc084d3979bb4f4
Jan Sitarski <jansitarski00@gmail.com>
2020-08-02 21:46:13 +0200

    /home/malar added

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 while getopts 'anc:' flag; do
   case "${flag}" in
-		n) WALLPAPER="$(find /home/malar/wallpapers/*.jpg -type f | shuf -n 1)";;	
-		a) WALLPAPER="$(find /home/malar/wallpapers/animeWallpapers/*.jpg -type f | shuf -n 1)";;
+		n) WALLPAPER="$(find $HOME/wallpapers/*.jpg -type f | shuf -n 1)";;	
+		a) WALLPAPER="$(find $HOME/wallpapers/animeWallpapers/*.jpg -type f | shuf -n 1)";;
 		c) WALLPAPER="$(find ${OPTARG}/*.jpg -type f | shuf -n 1)";;
 	esac
 feh --bg-scale $WALLPAPER
