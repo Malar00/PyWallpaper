8a14edffe26ec9436c8c19b9b7807879d899b78a
Jan Sitarski <jansitarski00@gmail.com>
2020-08-08 12:25:40 +0200

    wallpaper setter cli

--- pywallpaper.py
+++ pywallpaper.py
@@ -11,12 +11,26 @@ def download(title, url, directory):
         file.write(reqst.content)
 
 
+wallpapers = []
+
+
+def cli(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith('jpg'):
+            wallpapers.append(filename)
+    for wall in wallpapers:
+        print(str(wallpapers.index(wall)) + ') ' + wall)
+    choice = int(input("choose wallpaper from [0-" + str(len(wallpapers) - 1) + "]: "))
+    os.system("feh --bg-scale " + directory + wallpapers[choice])
+    os.system("wal -i " + directory + wallpapers[choice])
+
+
 reddit = praw.Reddit(client_id="pO9TuXycwz04UQ",
                      client_secret="NaGQ8jNp8KhsNB9gwQAdaiXjX_c",
                      user_agent="top 10 wallpapers of the day")
 
 argv = sys.argv[1:]
-opts, args = getopt.getopt(argv, 'l:r:')
+opts, args = getopt.getopt(argv, 'l:r:i')
 
 directory = './'
 subredd = "wallpapers"
@@ -24,8 +38,11 @@ subredd = "wallpapers"
 for op in opts:
     if op[0] == '-l':
         directory = op[1]
-    elif op[0] == '-r':
+    if op[0] == '-r':
         subredd = op[1]
+    if op[0] == '-i':
+        cli(directory)
+        exit()
 
 for submission in reddit.subreddit(subredd).hot(limit=10):
     for width in range(os.get_terminal_size()[0]):
@@ -34,3 +51,4 @@ for submission in reddit.subreddit(subredd).hot(limit=10):
     print(submission.url)
     pic_title = submission.title.replace('/', '_')
     download(pic_title.replace(' ', '_'), submission.url, directory)
+
